4bc99456f8337d0726dec2093d7f53e383b0503b
Tev969 <freezuka14@gmail.com>
2024-02-28 13:31:44 +0100

    finalisation portf

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const { config } = require('dotenv')
 const express = require('express')
 const mongoose = require('mongoose')
 const routerMain = require("./src/router/routerMain")
+const routerLogin = require("./src/router/routerLogin")
 require('dotenv').config()
 
 
 const app = express()
 app.use(express.static("./assets"))
+app.use(express.urlencoded({extended:true}))
 app.use(routerMain)
+app.use(routerLogin)
+
 
 app.listen(parseInt(process.env.PORT) , (err)=>{
      if (err) {
