0f00990ed8128b4ffc6702f6954f8fd3102357ae
Tev969 <freezuka14@gmail.com>
2024-02-28 13:32:07 +0100

    finalisation probelme protfollio

--- src/models/projectModels.js
+++ src/models/projectModels.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
    mail: {
      type: String,
-     required: [true , "prenom requis"]
+     required: [true , "prenom requis"],
+     unique: true,
+    validate: {
+        validator:function(v) {
+            return /^[\w-\.]+@(\[w-]{2,4}$/g.test(v);
+        },
+    },
+   }
+     
    },
    password: {
      type: String,
